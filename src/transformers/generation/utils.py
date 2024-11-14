85fae4f9106edb3d7fdd480f3211e7e88266c031
nrocketmann <nameerhirschkind@gmail.com>
2024-11-13 17:42:02 -0800

    Update utils.py

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -1593,6 +1593,7 @@ class GenerationMixin:
             or (not isinstance(cache_to_check, cache_cls))
             or cache_to_check.batch_size != batch_size
         )
+	need_new_cache = True
         if cache_implementation != "mamba":
             need_new_cache = need_new_cache or cache_to_check.max_cache_len < max_cache_len
 
